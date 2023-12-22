30c113e33e2d2d46054110e7a2dcf5d481a2c928
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2023-12-22 16:11:50 -0600

    ALL

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace BankEncapsulation
+﻿namespace Bank_Encapsulations
 {
-    public class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of the BankAccount class
+            BankAccount myAccount = new BankAccount();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("How much would you deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            myAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your Balance is now {myAccount.GetBalance()}");
         }
     }
 }
