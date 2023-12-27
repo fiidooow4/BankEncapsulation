3104d7c4cf1885151b5d9262747cfd0f20dc473f
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2023-12-27 15:45:35 -0600

    Add new Files

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Encapsulations
+{
+    public class BankAccount
+    {
+        // Private field
+        private double balance = 0;
+
+        // Method to deposit money
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+
+    }
+}
